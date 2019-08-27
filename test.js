264eb0e01170d3b72bd044634c096c34ef14d2f3
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-08-27 02:06:20 -0700

    Switching to typescript

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-/* eslint-disable flowtype/require-return-type */
+
 import {test} from "tap";
 
-import mergeWithKey from "./index";
+import mergeWithKey from "./";
 
 test(({same, end}) => {
   same(
