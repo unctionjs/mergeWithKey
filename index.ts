80bf919c1c22bc29bf2cfede9f7d52f06baafbf3
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-28 18:50:20 -0800

    Denormalizing some types to make things a little bit easier

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import reduceWithValueKey from "@unction/reducewithvaluekey";
 import attach from "@unction/attach";
 import {MapperFunctionType} from "./types";
-import {KeyedEnumerableType} from "./types";
 
 export default function mergeWithKey<K, L, R, V> (unction: MapperFunctionType<L, MapperFunctionType<R, MapperFunctionType<K, V>>>) {
   return reduceWithValueKey((accumulated: KeyedEnumerableType<V, K>) => (value: R) => (key: K) => {
